930420ff1c95561b0cc6beb6f085917c61472fa7
justin-caldicott <justin@caldicott.org>
2019-07-11 09:17:11 +0100

    test: better test reporting
    Don't buffer to a single string and log at end

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
 /* eslint-disable arrow-parens */
 
-// Not happy with intercepting console.log in this way, but couldn't seem to get stdout.pipe working
-const originalConsoleLog = console.log;
-let output = '';
-console.log = msg => {
-  output += `${msg}\n`;
-};
-
 const { test } = require('zora');
 const reporter = require('tap-mocha-reporter');
 
 const r = reporter('spec');
 test.indent();
+
+// Not happy with intercepting console.log in this way, but couldn't seem to get stdout.pipe working
+const originalConsoleLog = console.log;
+console.log = msg => {
+  const newConsoleLog = console.log;
+  console.log = originalConsoleLog;
+  r.write(`${msg}\n`);
+  console.log = newConsoleLog;
+};
 
 require('./src/serializers/jsonSerializer.test');
 require('./src/storage/memoryStorage.test');
@@ -20,6 +22,5 @@ require('./src/buffr.test');
 // Once all of the tests have finished running
 process.on('exit', () => {
   console.log = originalConsoleLog;
-  r.write(output);
   r.end();
 });
