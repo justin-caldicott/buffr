a6772642ee5f68acd2dfee88d9f7c499a56bb9b3
justin-caldicott <justin@caldicott.org>
2019-07-15 08:11:39 +0100

    test: simplify memoryStorage tests

--- src/storage/memoryStorage.test.js
+++ src/storage/memoryStorage.test.js
@@ -1,27 +1,21 @@
 /* eslint-disable no-shadow */
 /* eslint-disable arrow-parens */
 const { test } = require('zora');
-const textEncoding = require('text-encoding');
 const MemoryStorage = require('./memoryStorage');
 
-const { TextEncoder, TextDecoder } = textEncoding;
-
-const encoder = new TextEncoder('utf-8');
-const decoder = new TextDecoder('utf-8');
-
-const payload = 'foo bar';
-const payload2 = 'foo bar baz';
+const payload = new Uint8Array([1, 2, 3, 4, 5, 255]);
+const payload2 = new Uint8Array([10, 20, 30, 40, 50, 60, 70, 80, 90, 255]);
 
 test('memoryStorage', t => {
   t.test('when some data is stored', t => {
     const storage = new MemoryStorage();
-    t.equal(storage.add(encoder.encode(payload)), 0, 'adds to correct index');
-    t.equal(decoder.decode(storage.get(0)), payload, 'we get the same data back');
+    t.equal(storage.add(payload), 0, 'adds to correct index');
+    t.equal(storage.get(0), payload, 'we get the same data back');
   });
   t.test('when a second item is stored', t => {
     const storage = new MemoryStorage();
-    t.equal(storage.add(encoder.encode(payload)), 0, 'adds to correct index');
-    t.equal(storage.add(encoder.encode(payload2)), 1, 'adds to correct index');
-    t.equal(decoder.decode(storage.get(1)), payload2, 'we get the same data back');
+    t.equal(storage.add(payload), 0, 'adds to correct index');
+    t.equal(storage.add(payload2), 1, 'adds to correct index');
+    t.equal(storage.get(1), payload2, 'we get the same data back');
   });
 });
