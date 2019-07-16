d86d9f2d655d5831f7a3cb98256b17696066df48
justin-caldicott <justin@caldicott.org>
2019-07-16 21:55:21 +0100

    feat(memory-storage): clear, update

--- src/storage/memoryStorage.js
+++ src/storage/memoryStorage.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-param-reassign */
 class MemoryStorage {
   constructor() {
     const initialIndexCapacity = 4096;
     const headerSize = 4 * 4;
-    this.buffer = new ArrayBuffer(headerSize + (4 * initialIndexCapacity) + 65536);
+    this.buffer = new ArrayBuffer(headerSize + (4 * initialIndexCapacity) + 65536); // TODO: Start small and grow as needed
     this.bytes = new Uint8Array(this.buffer);
     const header = new Uint32Array(this.buffer, 0, 4);
     header[0] = 0;                                       // Size
@@ -15,11 +16,35 @@ class MemoryStorage {
   }
 
   /**
+   * Add data
    * @param {Uint8Array} data
    * @returns {number} index
    */
   add(data, capacity = null) {
     const index = this.header[0];
+    this._updateAtNewOffset(index, data, capacity);
+    this.header[0] = index + 1;
+    return index;
+  }
+
+  clear(index) {
+    const offset = this.index[index];
+    const itemHeader = new Uint32Array(this.buffer, offset, 2);
+    itemHeader[0] = 0;
+  }
+
+  update(index, data, capacity = null) {
+    const offset = this.index[index];
+    const itemHeader = new Uint32Array(this.buffer, offset, 2);
+    if (data.length <= itemHeader[1]) { // <= capacity
+      itemHeader[0] = data.length;
+      this.bytes.set(data, offset + 8);
+    } else {
+      this._updateAtNewOffset(index, data, capacity);
+    }
+  }
+
+  _updateAtNewOffset(index, data, capacity) {
     const offset = this.header[3];
     this.index[index] = offset; // TODO: Offsets as multiples of 16, to allow 32/64GB with 32-bits
     const itemHeader = new Uint32Array(this.buffer, offset, 2);
@@ -27,9 +52,7 @@ class MemoryStorage {
     capacity = capacity || data.length + (4 - (data.length % 4)); // Adds an extra 4 bytes if already a multiple of 4, but it's ok
     itemHeader[1] = capacity;
     this.bytes.set(data, offset + 8);
-    this.header[0] = this.header[0] + 1;
     this.header[3] = this.header[3] + 8 + capacity;
-    return index;
   }
 
   /**
--- src/storage/memoryStorage.test.js
+++ src/storage/memoryStorage.test.js
@@ -1,21 +1,83 @@
+/* eslint-disable max-len */
 /* eslint-disable no-shadow */
 /* eslint-disable arrow-parens */
+
 const { test } = require('zora');
 const MemoryStorage = require('./memoryStorage');
 
 const payload = new Uint8Array([1, 2, 3, 4, 5, 255]);
-const payload2 = new Uint8Array([10, 20, 30, 40, 50, 60, 70, 80, 90, 255]);
+const payload2 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]);
+const payload3 = new Uint8Array([5, 6, 7]);
+
+const isBasedOn = (capacity, length) => capacity >= length && capacity < (length + 8);
 
 test('memoryStorage', t => {
-  t.test('when some data is stored', t => {
+  t.test('when some data is added', t => {
     const storage = new MemoryStorage();
-    t.equal(storage.add(payload), 0, 'adds to correct index');
-    t.equal(storage.get(0), payload, 'we get the same data back');
+    t.equal(storage.add(payload), 0, 'returns the correct index');
+    t.equal(storage.get(0), payload, 'get returns the same data');
+    const data = storage.getRaw(0);
+    const bytes = new Uint8Array(data.buffer, data.offset, data.length);
+    t.equal(bytes, payload, 'getRaw returns the same data');
+    t.ok(isBasedOn(data.capacity, payload.length), 'getRaw returns capacity ~= length');
   });
-  t.test('when a second item is stored', t => {
+
+  t.test('when some data is added, with capacity set', t => {
     const storage = new MemoryStorage();
-    t.equal(storage.add(payload), 0, 'adds to correct index');
-    t.equal(storage.add(payload2), 1, 'adds to correct index');
-    t.equal(storage.get(1), payload2, 'we get the same data back');
+    t.equal(storage.add(payload, 128), 0, 'returns the correct index');
+    t.equal(storage.get(0), payload, 'get returns the same data');
+    t.equal(storage.getRaw(0).capacity, 128, 'getRaw returns the set capacity');
   });
+
+  t.test('when a second item is added', t => {
+    const storage = new MemoryStorage();
+    storage.add(payload);
+    t.equal(storage.add(payload2), 1, 'returns the correct index');
+    t.equal(storage.get(1), payload2, 'get returns the same data');
+  });
+
+  t.test('when data is cleared', t => {
+    const storage = new MemoryStorage();
+    storage.add(payload);
+    const before = storage.getRaw(0);
+    storage.clear(0);
+    const data = storage.getRaw(0);
+    t.equal(data.length, 0, 'getRaw returns zero length');
+    t.equal(data.capacity, before.capacity, 'getRaw returns the same capacity');
+  });
+
+  t.test('when data is updated to be smaller', t => {
+    const storage = new MemoryStorage();
+    storage.add(payload);
+    const before = storage.getRaw(0);
+    storage.update(0, payload3);
+    t.equal(storage.get(0), payload3, 'get returns the updated data');
+    const data = storage.getRaw(0);
+    t.equal(data.length, payload3.length, 'getRaw returns the updated length');
+    t.equal(data.capacity, before.capacity, 'getRaw returns the same capacity');
+  });
+
+  t.test('when data is updated to be larger', t => {
+    const storage = new MemoryStorage();
+    storage.add(payload);
+    storage.update(0, payload2);
+    t.equal(storage.get(0), payload2, 'get returns the updated data');
+    const data = storage.getRaw(0);
+    t.equal(data.length, payload2.length, 'getRaw returns the updated length');
+    t.ok(isBasedOn(data.capacity, payload2.length), 'getRaw returns capacity ~= updated length');
+  });
+
+  t.test('when data is updated to be larger, with capacity set', t => {
+    const storage = new MemoryStorage();
+    storage.add(payload);
+    storage.update(0, payload2, 128);
+    t.equal(storage.get(0), payload2, 'get returns the updated data');
+    const data = storage.getRaw(0);
+    t.equal(data.length, payload2.length, 'getRaw returns the updated length');
+    t.ok(isBasedOn(data.capacity, 128), 'getRaw returns the set capacity');
+  });
+
+  // TODO: Initial index size
+  // TODO: Index growth
+  // TOOD: Buffer growth
 });
